10deb637b92c4ae1d653f75a907673409a272c76
torus2016 <irisforpublic@gmail.com>
2019-11-06 23:41:05 +0300

    permissions

--- projects/1/model.py
+++ projects/1/model.py
@@ -17,7 +17,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 #
 
 # We create the preprocessing pipelines for both numeric and categorical data.
-numeric_features = ["if"+str(i) for i in range(1,14)]
+numeric_features = ["if"+str(i) for i in range(1,13)]
 numeric_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='median')),
 #    ('scaler', StandardScaler())
