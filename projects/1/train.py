93a084403923c704989934fa980b8b5091fe154b
torus2016 <irisforpublic@gmail.com>
2019-11-09 18:28:07 +0300

    model train permissions

--- projects/1/train.py
+++ projects/1/train.py
@@ -44,7 +44,7 @@ logging.info(f"TRAIN_PATH {train_path}")
 file_fields = ["id","label"] + ["if"+str(i) for i in range(1,14)] + ["cf"+str(i) for i in range(1,27)] + ["day_number"]
 
 
-read_table_opts = dict(sep="\t", file_fields, index_col=False)
+read_table_opts = dict(sep="\t", names = file_fields, index_col=False)
 df = pd.read_table(train_path, **read_table_opts)
 
 df2 = df.iloc[:,1]
