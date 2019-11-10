a9d00b3b06eea3109c085c3a2caf2888585ae2e4
torus2016 <irisforpublic@gmail.com>
2019-11-10 17:54:39 +0300

    comment

--- projects/1/filter_cond.py
+++ projects/1/filter_cond.py
@@ -1,10 +1,14 @@
 #
 #
+import pandas as pd
+import numpy as np
+
 def filter_cond(line_dict):
     """Filter function
     Takes a dict with field names as argument
     Returns True if conditions are satisfied
     """
+    line_dict["if1"] = line_dict["if1"].fillna(value=pd.np.nan, inplace=True)
     cond_match = (
        int(line_dict["if1"]) > 20 and int(line_dict["if1"]) < 40
     ) 
